0d3292d735d371de68b96797f68f09ee984fb34f
jacopomv <jac.valtorta@gmail.com>
2018-05-28 18:26:59 +0200

    initial commit

---
+++ app/src/main/java/com/example/jacopo/socket/Client.java
@@ -0,0 +1,311 @@
+package com.example.jacopo.socket;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.view.View;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import de.frank_durr.ecdh_curve25519.ECDHCurve25519;
+
+public class Client extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String PREFS_NAME = "MyPrefsFile";
+    public static final String TAG = ECDHCurve25519.class.getName();
+    volatile byte[] server_public_key;
+
+
+    static {
+        // Load native library ECDH-Curve25519-Mobile implementing Diffie-Hellman key
+        // exchange with elliptic curve 25519.
+        try {
+            System.loadLibrary("ecdhcurve25519");
+            Log.i(TAG, "Loaded ecdhcurve25519 library.");
+        } catch (UnsatisfiedLinkError e) {
+            Log.e(TAG, "Error loading ecdhcurve25519 library: " + e.getMessage());
+        }
+    }
+
+    static {
+        Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
+    }
+
+    public static final String TAG_MAIN = Client.class.getSimpleName();
+
+    //Encryption variables
+    String cipherText = "";
+    boolean createdKeys;
+    String plainText = "";
+    private TextView textViewAliceSharedSecret;
+    private TextView textViewBobSharedSecret;
+    private EditText msg;
+    //private Test test = new Test();
+    private SecretKey secretKeyA;
+    private byte[] client_secret_key;
+    private byte[] client_public_key;
+    byte[] client_shared_secret;
+
+    public static final int SERVERPORT = 3000;
+
+    public static final String SERVER_IP = "192.168.43.235";
+    ClientThread clientRunnable;
+    Thread thread;
+    TextView messageTv;
+    private final String TAG_pub_client="pub_client";
+    private boolean hasReceivedKey=false;
+
+    private void generateClientKeys() {
+
+        if (!createdKeys) {
+            // Create Client's secret key from a big random number.
+            SecureRandom random = new SecureRandom();
+            client_secret_key = ECDHCurve25519.generate_secret_key(random);
+            // Create Client's public key.
+            client_public_key = ECDHCurve25519.generate_public_key(client_secret_key);
+
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            SharedPreferences.Editor editor=sharedPref.edit();
+            try {
+                editor.putString(TAG_pub_client,new String(client_public_key,"UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            editor.commit();
+        } else
+            return;
+
+    }
+
+    public void generateShared(){
+        System.out.println("LUNGHEZZA:  "+server_public_key.length + "CLIENT" + client_secret_key.length);
+        client_shared_secret = ECDHCurve25519.generate_shared_secret(
+                client_secret_key, server_public_key);
+        try {
+            Log.i(TAG, "SHARED KEY: " + new String(client_shared_secret,"UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+        messageTv = (TextView) findViewById(R.id.messageTv);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        generateClientKeys();
+
+        clientRunnable = new ClientThread();
+        thread = new Thread(clientRunnable);
+        thread.start();
+
+
+        msg = (EditText) findViewById(R.id.message);
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("createdKeys", false);
+
+        editor.commit();
+
+       /* FloatingActionButton encrypt = (FloatingActionButton) findViewById(R.id.encrypt);
+        encrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                msg = (EditText) findViewById(R.id.message);
+                plainText = msg.getText().toString();
+                System.out.println("Original plaintext message: " + plainText);
+
+                // Encrypt the message using 'secretKeyA'
+                cipherText = test.encryptString(secretKeyA, plainText);
+                System.out.println("Encrypted cipher text: " + cipherText);
+
+                // Decrypt the message using 'secretKeyA'
+                String decryptedPlainText = test.decryptString(secretKeyA, cipherText);
+                System.out.println("Decrypted cipher text: " + decryptedPlainText);
+
+                textViewAliceSharedSecret = (TextView) findViewById(R.id.alice_secret);
+                textViewBobSharedSecret = (TextView) findViewById(R.id.bob_secret);
+
+                textViewAliceSharedSecret.setText(cipherText);
+                textViewBobSharedSecret.setText(decryptedPlainText);
+            }
+        });*/
+
+    }
+
+    public void updateMessage(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageTv.append(message + "\n");
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.connect_server) {
+            messageTv.setText("");
+            //clientRunnable = new ClientThread();
+            //thread = new Thread(clientRunnable);
+            //thread.start();
+            return;
+        }
+
+        /*if(view.getId() == R.id.send_shared_key){
+            Log.d(TAG, "Sendind public keys...");
+            thread.start();
+        }*/
+
+        if (view.getId() == R.id.send_data) {
+            SecretKey originalKey = new SecretKeySpec(client_shared_secret, 0, client_shared_secret.length, "AES");
+
+            String encrypted=Test.encryptString(originalKey,msg.getText().toString());
+            Log.i(TAG, "Message encrypted : " + encrypted);
+            clientRunnable.sendMessage(encrypted);
+        }
+    }
+
+    class ClientThread implements Runnable {
+
+        private Socket socket;
+        private DataInputStream input;
+
+        @Override
+        public void run() {
+
+            try {
+                System.out.println("IN RUN");
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+                socket = new Socket(serverAddr, SERVERPORT);
+                //PrintWriter out = new PrintWriter(new BufferedWriter(
+                        //new OutputStreamWriter(socket.getOutputStream())),
+                        //true);
+                //out.println(new String(client_public_key,"UTF-8"));
+                //out.println(client_public_key+"");
+                if(!hasReceivedKey){
+                    DataOutputStream d = new DataOutputStream(socket.getOutputStream());
+                    d.writeInt(client_public_key.length);
+                    d.write(client_public_key);
+                } else {
+                    PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())),
+                    true);
+                    out.println();
+                    out.println(client_public_key+"");
+
+                }
+
+
+
+                while (!Thread.currentThread().isInterrupted()) {
+
+                    Log.i(TAG, "Waiting for message from server...");
+
+                    this.input = new DataInputStream(socket.getInputStream());//String message = input.readLine();
+
+                    if(!hasReceivedKey){
+                        int length = input.readInt();
+                        if(length>0) {
+                            server_public_key = new byte[length];
+                            input.readFully(server_public_key, 0, server_public_key.length);
+                            while (server_public_key == null) {
+                                Thread.sleep(1000);
+                            }
+                        }
+                        System.out.println("FUORI DAL WHILE:"+ server_public_key);
+                        generateShared();
+                        System.out.println("LEONISIOOOO_CLIENT");
+
+                    } else {
+                        BufferedReader input_mex = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+                        String message = input_mex.readLine();
+                        //String mex=test.decryptString(secretKeyA,read);
+                        Log.i(TAG, "Message Received from Client : " + message);
+
+
+                        Log.i(TAG, "Message received from the server : " + message);
+
+                        if (null == message || "Disconnect".contentEquals(message)) {
+                            Thread.interrupted();
+                            message = "Server Disconnected.";
+                            updateMessage(getTime() + " | Server : " + message);
+                            break;
+                        }
+
+                        updateMessage(getTime() + " | Server : " + message);
+
+                    }
+                }
+
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        void sendMessage(String message) {
+            try {
+                if (null != socket) {
+                    PrintWriter out = new PrintWriter(new BufferedWriter(
+                            new OutputStreamWriter(socket.getOutputStream())),
+                            true);
+                    out.println(message);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+    }
+
+    String getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != clientRunnable) {
+            clientRunnable.sendMessage("Disconnect");
+            clientRunnable = null;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/jacopo/socket/MainActivity.java
@@ -0,0 +1,318 @@
+package com.example.jacopo.socket;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import de.frank_durr.ecdh_curve25519.ECDHCurve25519;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String PREFS_NAME = "MyPrefsFile";
+    public static final String TAG = ECDHCurve25519.class.getName();
+
+
+    //Encryption variables
+    String cipherText = "";
+    boolean createdKeys;
+    private Test test = new Test();
+
+    static {
+        // Load native library ECDH-Curve25519-Mobile implementing Diffie-Hellman key
+        // exchange with elliptic curve 25519.
+        try {
+            System.loadLibrary("ecdhcurve25519");
+            Log.i(TAG, "Loaded ecdhcurve25519 library.");
+        } catch (UnsatisfiedLinkError e) {
+            Log.e(TAG, "Error loading ecdhcurve25519 library: " + e.getMessage());
+        }
+    }
+
+    static {
+        Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
+    }
+
+    public static final String TAG_MAIN = MainActivity.class.getSimpleName();
+
+
+    private ServerSocket serverSocket;
+    private Socket tempClientSocket;
+    Thread serverThread = null;
+    public static final int SERVER_PORT = 3000;
+    TextView messageTv;
+    private String TAG_pub_server="pub_server";
+    byte[] server_public_key;
+    byte[] client_public_key;
+    byte[] server_secret_key;
+    byte[] server_shared_secret;
+    private boolean hasReceivedKey=false;
+
+    private void generateServerKeys() {
+
+        if (!createdKeys) {
+            // Create Client's secret key from a big random number.
+            SecureRandom random = new SecureRandom();
+
+            // Bob is also calculating a key pair.
+            server_secret_key = ECDHCurve25519.generate_secret_key(random);
+            server_public_key = ECDHCurve25519.generate_public_key(server_secret_key);
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            SharedPreferences.Editor editor=sharedPref.edit();
+            try {
+                editor.putString(TAG_pub_server,new String(server_public_key,"UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            editor.commit();
+
+        } else
+            return;
+
+    }
+    private void generateShared(){
+        //Generating Server shared key
+        System.out.println("LUNGHEZZA:  "+client_public_key.length+"SERVER LENGHT:"+server_secret_key);
+        server_shared_secret = ECDHCurve25519.generate_shared_secret(
+                server_secret_key, client_public_key);
+        try {
+            Log.i(TAG, "SHARED KEY: " + new String(server_shared_secret,"UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        messageTv = (TextView) findViewById(R.id.messageTv);
+
+        WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        String ip = MainActivity.getIPAddress(true);
+        //String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        System.out.println("IP ADD: "+ ip);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        generateServerKeys();
+        this.serverThread = new Thread(new ServerThread());
+        this.serverThread.start();
+
+    }
+
+    public void updateMessage(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageTv.append(message + "\n");
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {/*
+        if(view.getId() == R.id.send_shared_key){
+            Log.d(TAG, "Sendind public keys...");
+            this.serverThread.start();
+        }*/
+        if (view.getId() == R.id.send_data) {
+            sendMessage("Hello from Server...");
+        }
+        if(view.getId() == R.id.switchToC){
+            Intent intent=new Intent(MainActivity.this,Client.class);
+            MainActivity.this.startActivity(intent);
+        }
+    }
+
+    private void sendMessage(String message) {
+        try {
+            if (null != tempClientSocket) {
+                PrintWriter out = new PrintWriter(new BufferedWriter(
+                        new OutputStreamWriter(tempClientSocket.getOutputStream())),
+                        true);
+                out.println(message);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class ServerThread implements Runnable {
+
+        public void run() {
+            Socket socket;
+            try {
+                serverSocket = new ServerSocket(SERVER_PORT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (null != serverSocket) {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        socket = serverSocket.accept();
+                        CommunicationThread commThread = new CommunicationThread(socket);
+                        /*PrintWriter out = new PrintWriter(new BufferedWriter(
+                                new OutputStreamWriter(tempClientSocket.getOutputStream())),
+                                true);
+                        out.println(new String(server_public_key,"UTF-8"));*/
+                        DataOutputStream d = new DataOutputStream(socket.getOutputStream());
+                        d.writeInt(server_public_key.length);
+                        d.write(server_public_key);
+                        new Thread(commThread).start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    class CommunicationThread implements Runnable {
+
+        private Socket clientSocket;
+
+        private DataInputStream input;
+
+        public CommunicationThread(Socket clientSocket) {
+
+            this.clientSocket = clientSocket;
+            tempClientSocket = clientSocket;
+
+            try {
+                this.input = new DataInputStream(this.clientSocket.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            updateMessage("Server Started...");
+        }
+
+        public void run() {
+
+            while (!Thread.currentThread().isInterrupted()) {
+
+                try {
+
+
+                    if(!hasReceivedKey){
+                        int length = input.readInt();
+                        if(length>0){
+                            client_public_key = new byte[length];
+                            input.readFully(client_public_key,0,client_public_key.length);
+                        }
+                        //client_public_key=read;
+
+                        Log.i(TAG, "Message received from the client : " + client_public_key);
+                        hasReceivedKey=true;
+
+                        while(client_public_key==null) {
+
+                        }
+                        generateShared();
+                        System.out.println("LEONISIOOOO");
+
+                    }else {
+
+                        BufferedReader input_mex = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+                        String read = input_mex.readLine();
+                        SecretKey originalKey = new SecretKeySpec(server_shared_secret, 0, server_shared_secret.length, "AES");
+                        String mex=test.decryptString(originalKey,read);
+                        Log.i(TAG, "Message Received from Client : " + mex);
+
+                        if (null == read || "Disconnect".contentEquals(read)) {
+                            Thread.interrupted();
+                            read = "Client Disconnected";
+                            updateMessage(getTime() + " | Client : " + read);
+                            break;
+                        }
+                        updateMessage(getTime() + " | Client : " + "Message encrypted: "+read);
+                        updateMessage(getTime() + " | Client : " + "Message decrypted: "+mex);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+    String getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != serverThread) {
+            sendMessage("Disconnect");
+            serverThread.interrupt();
+            serverThread = null;
+        }
+    }
+    public static String getIPAddress(boolean useIPv4) {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = sAddr.indexOf(':')<0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) { } // for now eat exceptions
+        return "";
+    }
+}
